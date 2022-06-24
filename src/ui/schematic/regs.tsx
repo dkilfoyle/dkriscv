926a2832f5b565612a680ee829545125b30140eb
dkilfoyle <dkilfoyle@xtra.co.nz>
2022-06-24 21:02:12 +1200

    reg buttons

--- src/ui/schematic/regs.tsx
+++ src/ui/schematic/regs.tsx
@@ -1,4 +1,5 @@
-import { Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import { Button, ButtonGroup, HStack, Spacer, Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import { useState } from "react";
 import { registerNames } from "../../assemblers/riscv/builder";
 import { Processor } from "../../simulator/Processor";
 
@@ -20,22 +21,35 @@ const regColor = (i) => {
 };
 
 export const Regs = (props: { cpu: Processor }) => {
+  const [format, setFormat] = useState(10);
   return (
     <TableContainer>
       <Table size="xs">
         <Thead>
           <Tr>
-            <Th></Th>
-            <Th>Regs</Th>
-            <Th></Th>
+            <Th colSpan={3}>
+              <HStack>
+                <span style={{ paddingLeft: "10px" }}>Regs</span> <Spacer></Spacer>
+                <ButtonGroup size="xs" isAttached variant="outline">
+                  <Button onClick={() => setFormat(10)} size="xs">
+                    D
+                  </Button>
+                  <Button onClick={() => setFormat(16)} size="xs">
+                    H
+                  </Button>
+                </ButtonGroup>
+              </HStack>
+            </Th>
           </Tr>
         </Thead>
         <Tbody fontFamily="monospace">
           {props.cpu.x.map((r, i) => (
             <Tr key={i} style={{ background: regColor(i) }}>
-              <td>x{i}</td>
-              <td>{props.cpu.x[i]}</td>
-              <td>{registerNames[i]}</td>
+              <td align="right">x{i}</td>
+              <td align="center" style={{ minWidth: 50 }}>
+                {format === 10 ? props.cpu.x[i] : "0x" + props.cpu.x[i].toString(16)}
+              </td>
+              <td align="left">{registerNames[i]}</td>
             </Tr>
           ))}
         </Tbody>
